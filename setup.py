e1b716fc666e747511556945da231e9fe5a4cc25
Keep-Konect <66421869+konect-V@users.noreply.github.com>
2024-11-07 17:41:36 +0100

    Update

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 import subprocess
+import os
 
 is_kiauh = 0
 
 def install_kiauh():
     global is_kiauh
-    
+
     if is_kiauh == 1:
         return
     
@@ -16,8 +17,13 @@ def install_kiauh():
         subprocess.run(["sudo", "apt-get", "install", "git", "-y"], check=True)
         
         # Step 2: Clone KIAUH repository into the home directory
-        print("Cloning KIAUH repository into the home directory...")
-        subprocess.run(["git", "clone", "https://github.com/dw-0/kiauh.git", "~"], check=True)
+        kiauh_path = os.path.expanduser("~/kiauh")
+        if not os.path.isdir(kiauh_path):
+            print("Cloning KIAUH repository into the home directory...")
+            subprocess.run(["git", "clone", "https://github.com/dw-0/kiauh.git", kiauh_path], check=True)
+            print("KIAUH installation completed.")
+        else:
+            print("KIAUH is already installed in the home directory.")
         
         print("KIAUH installation completed.")
     except subprocess.CalledProcessError as e:
