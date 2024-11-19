423730d120700c6d6b1542c78a9b22562571c6d3
konect-V <66421869+konect-V@users.noreply.github.com>
2024-11-19 22:05:00 +0100

    Update

--- scripts/autofirmware.py
+++ scripts/autofirmware.py
@@ -5,7 +5,7 @@ import time
 
 def get_canbus_uuid():
     # Commande à exécuter
-    command = "~/klippy-env/bin/python ~/klipper/scripts/canbus_query.py can0"
+    command = "/home/minifab/klippy-env/bin/python /home/minifab/klipper/scripts/canbus_query.py can0"
     
     try:
         # Exécution de la commande
